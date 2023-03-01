c56b8be7a24c4bff73c2734863dff07e18a9afa6
Serhii Kulbashnyi <twitch.live.ua@gmail.com>
2023-03-01 22:38:49 +0200

    Adds a solution

---
+++ src/fetchCountries.js
@@ -0,0 +1,73 @@
+import Notiflix from 'notiflix';
+
+const countriesList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+function fetchCountries(value) {
+  fetch(
+    `https://restcountries.com/v3.1/name/${value}?fields=name,capital,population,flags,languages`
+  )
+    .then(response => {
+      if (!response.ok) {
+        clearCountryInfo();
+        clearCountriesList();
+        throw new Error(
+          Notiflix.Notify.failure('Oops, there is no country with that name')
+        );
+      }
+      return response.json();
+    })
+    .then(data => {
+      if (data.length > 10) {
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+        clearCountriesList();
+      } else if (data.length >= 2 && data.length <= 10) {
+        createCountryList(data);
+        clearCountryInfo();
+      } else {
+        createCountryInfo(data);
+        clearCountriesList();
+      }
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+function clearCountriesList() {
+  countriesList.innerHTML = '';
+}
+
+function clearCountryInfo() {
+  countryInfo.innerHTML = '';
+}
+
+function createCountryList(arrayCountriesName) {
+  const markup = arrayCountriesName
+    .map(({ name, flags }) => {
+      return `<li class="country-item"><img class="country-image" src="${flags.svg}" alt="${flags.alt}" width="25" height="15"><span>${name.common}</span></li>`;
+    })
+    .join('');
+  countriesList.innerHTML = markup;
+}
+
+function createCountryInfo(countryName) {
+  const markup = countryName
+    .map(({ name, flags, capital, population, languages }) => {
+      return `
+      <p ><img class="country-image" src="${flags.svg}" alt="${
+        flags.alt
+      }" width="25" height="15">${name.common}</p>
+      <p><b class="country-text">Capital:</b>${capital}</p>
+      <p><b class="country-text">Population:</b>${population}</p>
+      <p><b class="country-text">Languages:</b>${Object.values(languages).join(
+        ', '
+      )}</p>`;
+    })
+    .join('');
+  countryInfo.innerHTML = markup;
+}
+
+export { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './fetchCountries.js';
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+const inputField = document.querySelector('input#search-box');
+
+inputField.addEventListener('input', debounce(onFormInput, DEBOUNCE_DELAY));
+
+function onFormInput() {
+  fetchCountries(inputField.value.trim());
+}
